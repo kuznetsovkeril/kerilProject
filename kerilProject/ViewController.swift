27223c5f3d979b7e064bde4b70d7726693146209
keril <keril@MacBook-Pro-apple.local>
2025-08-08 11:37:40 +0300

    initial commit

---
+++ kerilProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kerilProject
+//
+//  Created by keril on 08.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
