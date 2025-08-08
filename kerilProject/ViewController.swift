e95a1b686aad8b6fc2c63fa1855a87f309a368da
keril <keril@MacBook-Pro-apple.local>
2025-08-08 12:25:33 +0300

    lesson 4

--- kerilProject/ViewController.swift
+++ kerilProject/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     
     var name: String = "Keril"
+    var surname: String = "Kuznetsov"
+    var fullName: String { "\(name) \(surname)" }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Меня зовут \(name), я создал свое первое приложениеname")
+        print("Меня зовут \(fullName), я создал свое первое приложениеname")
     }
 
 
