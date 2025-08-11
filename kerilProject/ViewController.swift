3e5c55939749228210aa40382656c7c702fb35ea
keril <keril@MacBook-Pro-apple.local>
2025-08-11 17:59:34 +0300

    lesson 12

--- kerilProject/ViewController.swift
+++ kerilProject/ViewController.swift
@@ -11,20 +11,32 @@ class ViewController: UIViewController {
     private let helper = Helper()
     private let userRepository = UserRepository()
     private var users: [User] = []
+    private let fullNameLabel = UILabel()
+    private let someButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Show FullName", for: .normal)
+        button.backgroundColor = .green
+        button.frame = CGRect(x: 150, y: 100, width: 150, height: 50)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
-        view.alpha = 0.9
+        view.backgroundColor = .gray
         
         users = userRepository.getUsers()
         helper.addUsers(users: users)
-        showFullName(users: helper.getPerson())
+        
+        setupLabel()
+        view.addSubview(fullNameLabel)
+        view.addSubview(someButton)
     }
-    
-    private func showFullName(users: [User]) {
-        for user in users {
-            print(user.personalData.fullName)
-        }
+
+    private func setupLabel() {
+        let fullName = users.randomElement()?.personalData.fullName
+        fullNameLabel.text = fullName
+        fullNameLabel.font = .systemFont(ofSize: 25, weight: .bold)
+        fullNameLabel.textColor = .blue
+        fullNameLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
     }
 }
