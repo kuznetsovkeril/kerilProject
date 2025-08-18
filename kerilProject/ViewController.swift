da0365bb44b6ecc58ba8407a842cdc207c26f6e3
keril <keril@MacBook-Pro-apple.local>
2025-08-18 13:31:16 +0300

    lesson 16

--- kerilProject/ViewController.swift
+++ kerilProject/ViewController.swift
@@ -8,17 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     private let helper = Helper()
     private let userRepository = UserRepository()
     private var users: [User] = []
     private let fullNameLabel = UILabel()
-    private let someButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Show FullName", for: .normal)
-        button.backgroundColor = .green
-        button.frame = CGRect(x: 150, y: 100, width: 150, height: 50)
-        return button
-    }()
+    private let someButton = UIButton()
+    private let stackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,15 +24,47 @@ class ViewController: UIViewController {
         helper.addUsers(users: users)
         
         setupLabel()
-        view.addSubview(fullNameLabel)
-        view.addSubview(someButton)
+        setupButton()
+        setupStackView()
+        view.addSubview(stackView)
+        setupLayout()
     }
 
     private func setupLabel() {
         let fullName = users.randomElement()?.personalData.fullName
         fullNameLabel.text = fullName
-        fullNameLabel.font = .systemFont(ofSize: 25, weight: .bold)
-        fullNameLabel.textColor = .blue
-        fullNameLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
+        fullNameLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        fullNameLabel.textColor = .black
+        fullNameLabel.textAlignment = .center
+    }
+    
+    private func setupButton() {
+        someButton.setTitle("Randomize", for: .normal)
+        someButton.backgroundColor = .black
+        someButton.layer.cornerRadius = 10
+    }
+    
+    private func setupStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 0
+        
+        stackView.addArrangedSubview(fullNameLabel)
+        stackView.addArrangedSubview(someButton)
+    }
+    
+    private func setupLayout() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            stackView.heightAnchor.constraint(equalToConstant: 60),
+            
+            someButton.widthAnchor.constraint(equalToConstant: 130),
+            someButton.heightAnchor.constraint(equalToConstant: 40),
+        ])
     }
 }
